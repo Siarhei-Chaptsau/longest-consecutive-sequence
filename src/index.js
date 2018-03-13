764a74dbde212cae35fe64f31dfe46b099fcc6a1
Siarhei <s.chaptsau@gmail.com>
2018-03-13 03:10:54 +0300

    git commit -m "Implemented function"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  var count = 1;
+  var maxCount = 1;
+
+  array.sort(function(a, b) {
+    return a - b;
+  });
+
+  if (!array.length) {
+    maxCount = 0;
+  }
+
+  for (let i = array.length; i > 0; i--) { // удаление от конца массива дубликатов
+    if (array[i + 1] == array[i]) {
+      array.splice(i, 1);
+    }
+  }
+
+  array.forEach(function(item, i, array) {
+    if (array[i + 1] - array[i] == 1) {
+      count++;
+    }
+    if (array[i + 1] - array[i] != 1) {
+      if (count > maxCount) {
+        maxCount = count;
+      }
+      count = 1;
+    }
+  });
+
+  return maxCount;
 }
